c8f5a17a5c3c32168832915901961f88ced06e30
wuxinshuang <xinshuang.wu@chetailian.com>
2020-05-14 23:46:36 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,40 @@
 module.exports = {
-    base: '/blog/'
+    base: '/blog/',
+    title: '前端博客',  // 设置网站标题
+    description : '菜鸟日记',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ],
+    serviceWorker: true,
+    themeConfig: {
+        //导航栏
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '文章', link: '/form/form' },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/anick-xs/blog'
+            }
+        ],
+        //侧边栏
+        sidebar:{
+            '/form': [
+                {
+                    title:'javaScript',
+                    collapsable: true,
+                    children: [
+                        ['/form/form', 'form'],
+                    ]
+                },
+                {
+                    title:'算法',
+                    collapsable: true,
+                    children: [
+                        ['/form/algorithm/1', '两数之和'],
+                    ]
+                }
+            ]
+        }
+    }
 }
--- index.md
+++ index.md
@@ -1,1 +1,15 @@
-'# Hello VuePress! 123' 
+---
+home: true
+heroImage: /hero.png
+actionText: 起步 go →
+actionLink: /form/form
+features:
+- title: 模块化
+  details: 以模块化格式开发页面，以实现最简化的配置，帮助您专注于创作。
+- title: 配置化
+  details: 配置开发页面，配置组件自动生成你需要的页面。
+- title: 高效化
+  details: 有效地处理各种问题，一个更改将随其他页面而改变，无需单独处理。
+
+footer: MIT Licensed | Copyright © anicks
+---
